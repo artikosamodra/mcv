5c9a6d8d020fcf95021b85a52b02d2cf969497bf
artikosamodra <artikosamudra@gmail.com>
2023-12-05 11:22:35 +0700

    Fix Content, Un-fix Design, Un-Fix Animation.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
 function showPage(pageNumber, e) {
-    e.preventDefault(); // Mencegah perpindahan halaman
+  e.preventDefault(); // Mencegah perpindahan halaman
 
-    // Hide all card containers
-    document.getElementById('page1-cards').style.display = 'none';
-    document.getElementById('page2-cards').style.display = 'none';
-    document.getElementById('page3-cards').style.display = 'none';
+  // Hide all card containers
+  document.getElementById("page1-cards").style.display = "none";
+  document.getElementById("page2-cards").style.display = "none";
+  document.getElementById("page3-cards").style.display = "none";
 
-    // Remove 'active' class from all page items
-    document.getElementById('page1').classList.remove('active');
-    document.getElementById('page2').classList.remove('active');
-    document.getElementById('page3').classList.remove('active');
+  // Remove 'active' class from all page items
+  document.getElementById("page1").classList.remove("active");
+  document.getElementById("page2").classList.remove("active");
+  document.getElementById("page3").classList.remove("active");
 
-    // Show the selected page
-    document.getElementById('page' + pageNumber + '-cards').style.display = 'block';
+  // Show the selected page
+  document.getElementById("page" + pageNumber + "-cards").style.display =
+    "block";
 
-    // Add 'active' class to the selected page item
-    document.getElementById('page' + pageNumber).classList.add('active');
-  }
+  // Add 'active' class to the selected page item
+  document.getElementById("page" + pageNumber).classList.add("active");
+}
+
+// Membuat objek Intersection Observer
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // Jika elemen masuk ke dalam viewport
+    if (entry.isIntersecting) {
+      // Mulai animasi
+      entry.target.style.animationPlayState = "running";
+    } else {
+      // Hentikan animasi
+      entry.target.style.animationPlayState = "paused";
+    }
+  });
+});
+
+// Mengamati elemen dengan kelas "animated-element"
+// const animatedElement = document.querySelector(".bg-neon");
+// observer.observe(animatedElement);
